5d69a79b098c0041ea8cb013b2d2a6322b3688ec
T0lst0v <serghei.tolstov@gmail.com>
2022-02-10 23:59:38 -0500

    working profile Graph

--- public/js/app_profile.js
+++ public/js/app_profile.js
@@ -6,42 +6,58 @@ const gameMulti = document.getElementById("gameMulti");
 const btnMulti = document.getElementById("btnMulti");
 const multiStats = document.getElementById("multiStats");
 const soloStats = document.getElementById("soloStats");
+const labelTotalMulti = document.getElementById("labelTotalMulti");
+const labelTotal = document.getElementById("labelTotal");
 
 let win = chartWin.value;
 let loss = chartLoss.value;
 
+let myChart = displayChart(win, loss);
+
 btnMulti.addEventListener("click", () => {
   soloStats.style.display = "none";
   multiStats.style.display = "block";
-  win = 50;
-  loss = 50;
+  labelTotal.style.display = "none";
+  labelTotalMulti.style.display = "block";
+  myChart.destroy();
+  myChart = displayChart(chartWinMulti.value, chartLossMulti.value);
   // tableContainer.innerHTML = tableBuild(game);
 });
 
 btnSolo.addEventListener("click", () => {
   multiStats.style.display = "none";
   soloStats.style.display = "block";
-  // tableContainer.innerHTML = tableBuild(game);
+  labelTotal.style.display = "block";
+  labelTotalMulti.style.display = "none";
+  myChart.destroy();
+  myChart = displayChart(chartWin.value, chartLoss.value);
 });
 
-const data = {
-  labels: [win + " Wins", loss + " Losses"],
-  datasets: [
-    {
-      label: "My First Dataset",
-      data: [chartWin.value, chartLoss.value],
-      backgroundColor: ["rgb(157, 177, 113)", "rgb(176, 81, 75)"],
-      hoverOffset: 4,
+function changeStats(win, loss) {
+  const data = {
+    labels: [win + " Wins", loss + " Losses"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [win, loss],
+        backgroundColor: ["rgb(157, 177, 113)", "rgb(176, 81, 75)"],
+        hoverOffset: 4,
+      },
+    ],
+  };
+  return data;
+}
+function updateConfig(win, loss) {
+  const config = {
+    type: "doughnut",
+    data: changeStats(win, loss),
+    options: {
+      responsive: true,
     },
-  ],
-};
-
-const config = {
-  type: "doughnut",
-  data: data,
-  options: {
-    responsive: true,
-  },
-};
-
-const myChart = new Chart(ctx, config);
+  };
+  return config;
+}
+function displayChart(win, loss) {
+  const myChart = new Chart(ctx, updateConfig(win, loss));
+  return myChart;
+}
